21369f4df90b0719438b63169447b2a896161863
Suraj Nimbalkar <136218136+surajnimbalkar07@users.noreply.github.com>
2025-11-13 22:58:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+  let r = document.getElementById("radius").value.trim(); // take input as text
+  let volumeField = document.getElementById("volume");
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+  let radius = parseFloat(r);
+
+  // Check if radius is not a valid number or negative
+  if (isNaN(radius) || radius < 0) {
+    volumeField.value = "NaN";
+  } else {
+    let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    volumeField.value = volume.toFixed(4);
+  }
+}
+
+document.getElementById("calcBtn").addEventListener("click", volume_sphere);
